01583653a0f0ec3d3f76512e0c4dd91280f23c9f
Nischay Dhankhar <59060430+Nischaydnk@users.noreply.github.com>
2023-08-09 12:38:42 +0530

    Update htc_resnext101_exp1pret_wsiall.py

--- all_configs/nops_config_pret/htc_resnext101_exp1pret_wsiall.py
+++ all_configs/nops_config_pret/htc_resnext101_exp1pret_wsiall.py
@@ -329,8 +329,8 @@ data = dict(
         type='CocoDataset',
         classes=('blood_vessels', ),
         ann_file=
-        '/home/nischay/hubmap/coco/ds2wsiall_coco_1024_train_fold1.json',
-        img_prefix='/home/nischay/hubmap/Data/train/',
+        'coco_data/coco/ds2wsiall_coco_1024_train_fold1.json',
+        img_prefix='train/',
         pipeline=[
             dict(type='LoadImageFromFile'),
             dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
@@ -391,8 +391,8 @@ data = dict(
     val=dict(
         type='CocoDataset',
         classes=('blood_vessels', ),
-        ann_file='/home/nischay/hubmap/coco/ds1_coco_1024_valid_all_fold1.json',
-        img_prefix='/home/nischay/hubmap/Data/train/',
+        ann_file='coco_data/coco/ds1_coco_1024_valid_all_fold1.json',
+        img_prefix='train/',
         pipeline=[
             dict(type='LoadImageFromFile'),
             dict(
@@ -416,9 +416,8 @@ data = dict(
     test=dict(
         type='CocoDataset',
         classes=('blood_vessels', ),
-        ann_file=
-        '/home/nischay/hubmap/coco/ds12_coco_1024_valid_all_fold1.json',
-        img_prefix='/home/nischay/hubmap/Data/train/',
+        ann_file='coco_data/coco/ds1_coco_1024_valid_all_fold1.json',
+        img_prefix='train/',
         pipeline=[
             dict(type='LoadImageFromFile'),
             dict(
@@ -457,7 +456,7 @@ gpu_ids = range(0, 3)
 seed = 69
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-load_from = '/home/nischay/hubmap/try_mm/mmdetection/cbnet/detec_htcres101x32_pretcoco.pth'
+load_from = './detec_htcres101x32_pretcoco.pth'
 work_dir = './work_dirs/hubmap/htc_resnext101_exp1pret_wsiall'
 workflow = [('train', 1)]
 auto_resume = False
